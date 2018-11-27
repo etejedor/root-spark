839b21e08b903c71cf8d9a58cbdbc9dcd97cb479
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2018-11-27 09:07:21 +0100

    Optimize entry-based splitting in DistROOT
    - Make use of RDataFrame rather than TTreeReader
    - Consider cluster boundaries
    - Minimize TChain sweep in distributed workers
    - Explicitly defined range of entries in TChain to avoid cluster
    prefetching
    - Add old DistROOT version based on files (less flexible splitting)

--- DistROOT.py
+++ DistROOT.py
@@ -2,6 +2,7 @@
 import ROOT
 from pyspark import SparkConf, SparkContext, SparkFiles
 
+DEBUG = False
 
 ######################################################################
 # Function to initialize the Spark context. We need to configure:    #
@@ -57,15 +58,12 @@ class DistTree(object):
 
   def __init__(self, filelist, treename, npartitions):
     # Get number of entries and build the ranges according to npartitions
-    chain = ROOT.TChain(treename)
-    for fname in filelist:
-      chain.Add(fname)
-    nentries = chain.GetEntries()
-    ranges = BuildRanges(nentries, npartitions, chain)
+    nfiles = len(filelist)
+    ranges = BuildRanges(nfiles, npartitions, filelist, treename)
 
     # Initialize Spark context
     sc = InitSpark()
-  
+
     # Parallelize the ranges with Spark
     self.ranges = sc.parallelize(ranges, npartitions)
 
@@ -74,7 +72,7 @@ class DistTree(object):
     mapperIsCpp  = isinstance(fMap, ROOT.MethodProxy)
     reducerIsCpp = isinstance(fReduce, ROOT.MethodProxy)
     mapperName = reducerName = mapperCode = reducerCode = None
-   
+
     if mapperIsCpp:
       mapperName  = fMap.func_code.co_name
       mapperCode  = GetWrappedFunctionCode(mapperName)
@@ -97,28 +95,65 @@ class DistTree(object):
 
     def mapWrapper(rg):
       import ROOT
+
       ROOT.TH1.AddDirectory(False)
-      reader = ROOT.TTreeReader(rg.chain)
-      reader.SetEntriesRange(rg.start, rg.end)
+
+      start = int(rg.start)
+      end = int(rg.end)
+
+      #return [(start,end)]
+
+      chain = ROOT.TChain("TotemNtuple")
+      for f in rg.filelist:
+        chain.Add(f)
+      # We assume 'end' is exclusive
+      chain.SetCacheEntryRange(start, end)
+
+      tdf = ROOT.ROOT.RDataFrame(chain)
+      tdf_r = tdf.Range(start, end)
 
       if mapperIsCpp:
         ROOT.gInterpreter.Declare(mapperCode)
-        res = ROOT.__getattr__(mapperName)(reader)
+        res =  ROOT.__getattr__(mapperName)(tdf_r)
       else:
-        res = fMap(reader)
+        res = fMap(tdf_r)
 
-      return res
+      ## SHARED PTR TO PROXIED OBJECT, DOES NOT WORK
+      ## Caused by: java.io.EOFException
+      ## at java.io.DataInputStream.readInt(DataInputStream.java:392)
+      #ref = res.GetValue()
+      #h = ROOT.std.shared_ptr("TH1D")()
+      #h.reset(ROOT.AddressOf(ref))
+      #return h
+
+      # Quick hack with extra copies and just checking for TH1F and TH2F return values
+      if isinstance(res, list):
+        return [ ROOT.TH1D(h.GetValue()) if isinstance(h.GetValue(), ROOT.TH1D) else ROOT.TH2D(h.GetValue()) for h in res ]
+      else:
+        if isinstance(res.GetValue(), ROOT.TH1D): return ROOT.TH1D(res.GetValue())
+        else:                                     return ROOT.TH2D(res.GetValue())
+
+      ## Trigger the event loop, then return the proxy
+      ## DOES NOT WORK:
+      ## Caused by: java.io.EOFException
+      ## at java.io.DataInputStream.readInt(DataInputStream.java:392)
+      #if isinstance(res, list):
+      #  if res: res[0].GetValue()
+      #else:
+      #  res.GetValue()
+      #return res
 
     def reduceWrapper(x, y):
+      #return x + y
       if reducerIsCpp:
         import ROOT
         ROOT.gInterpreter.Declare(reducerCode)
         return ROOT.__getattr__(reducerName)(x, y)
       else:
         return fReduce(x, y) 
-      
+
     return self.ranges.map(mapWrapper).treeReduce(reduceWrapper)
- 
+
   def GetPartitions(self):
     return self.ranges.collect()
 
@@ -129,28 +164,81 @@ class DistTree(object):
 # and is the basis for parallelization.                            #
 ####################################################################
 
-def BuildRanges(nentries, npartitions, chain):
+def GetClusters(filelist, treename):
+  import ROOT
+
+  clusters = []
+  offset = 0
+  for filename in filelist:
+    f = ROOT.TFile.Open(filename)
+    t = f.Get(treename)
+
+    entries = t.GetEntriesFast()
+    it = t.GetClusterIterator(0)
+    start = it()
+    end = 0
+
+    while start < entries:
+      end = it()
+      clusters.append((start + offset, end + offset, offset, filename))
+      start = end
+
+    offset += entries
+
+  return clusters
+
+def BuildRanges(nfiles, npartitions, filelist, treename):
+  clusters = GetClusters(filelist, treename)
+  numclusters = len(clusters)
+  partSize = numclusters / npartitions
+  remainder = numclusters % npartitions
+
+  if DEBUG:
+    print("Num clusters", numclusters)
+    print("Partsize", partSize)
+    print("Remainder", remainder)
+    print("Initial clusters", clusters)
+
   i = 0
   ranges = []
-  partSize = nentries / npartitions
-    
-  while i < nentries:
-    start = i
+  entries_to_process = 0
+  while i < numclusters:
+    index_start = i
+    start = clusters[i][0]
     i = i + partSize
-    if i > nentries:
-      end = nentries
+    if remainder > 0:
+      i += 1
+      remainder -= 1
+    index_end = i
+    if i == numclusters:
+      end = clusters[-1][1]
     else:
-      end = i
-    ranges.append(Range(start, end, chain))
+      end = clusters[i-1][1]
+
+    range_files = []
+    for idx in range(index_start, index_end):
+      current_file = clusters[idx][3]
+      if range_files and range_files[-1] == current_file:
+        continue
+      range_files.append(clusters[idx][3])
+
+    offset_first_cluster = clusters[index_start][2]
+    ranges.append(Range(start - offset_first_cluster, end - offset_first_cluster, range_files))
+    entries_to_process += (end - start)
+
+  if DEBUG:
+    print("Entries to process", entries_to_process)
+    print("Final ranges", ranges)
 
   return ranges
 
-
 class Range(object):
-  def __init__(self, start, end, chain):
+  def __init__(self, start, end, filelist):
     self.start = start
     self.end = end
-    self.chain = chain
+    self.filelist = filelist
 
   def __repr__(self):
-    return "(" + str(self.start) + "," + str(self.end - 1) + ")"
+    return "(" + str(self.start) + "," + str(self.end) + "), " + str(self.filelist)
+
+
